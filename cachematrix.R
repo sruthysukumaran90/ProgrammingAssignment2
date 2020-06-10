a04f692f92aaba108f08f4aa828d214fcc0dbcce
sruthysukumaran90 <65181335+sruthysukumaran90@users.noreply.github.com>
2020-06-10 14:44:54 +0530

    Update cachematrix.R
    updated Result

--- cachematrix.R
+++ cachematrix.R
@@ -35,3 +35,19 @@ cacheSolve <- function(x, ...) {
   x$setInverse(inv)
   inv
 }
+#Result
+> x=rbind(c(1,-1/4),c(-1/4,1))
+> m=makeCacheMatrix(x)
+> m$get()
+      [,1]  [,2]
+[1,]  1.00 -0.25
+[2,] -0.25  1.00
+> cacheSolve(m)
+          [,1]      [,2]
+[1,] 1.0666667 0.2666667
+[2,] 0.2666667 1.0666667
+> cacheSolve(m)
+getting cached data.
+          [,1]      [,2]
+[1,] 1.0666667 0.2666667
+[2,] 0.2666667 1.0666667
